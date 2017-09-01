eb691fafc70d1bd860fde90d64b489512bab257b
win 10 <win 10@DESKTOP-K8NV66R>
2017-09-01 18:54:44 +0800

    aaa

--- recruit/src/main/java/org/recruit/Application.java
+++ recruit/src/main/java/org/recruit/Application.java
@@ -8,6 +8,6 @@ public class Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World1" );
     }
 }
